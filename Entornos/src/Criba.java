e73f4291b5deb93a2d94d84a493b6f4e27a7cbdb
usuario <usuario@LAPTOP-IHHK7HQF>
2021-03-01 21:05:42 +0100

    Commit de refactorizacion

--- Entornos/src/Criba.java
+++ Entornos/src/Criba.java
@@ -47,21 +47,30 @@ public class Criba
 	
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
+		
 		System.out.println("Introduce el número para la criba de Erastótenes:");
+		
 		int dato=teclado.nextInt();
 		int vector[]=new int[dato];
+		
 		System.out.println("\nVector inicial hasta :"+dato);
+		
 		for (int i = 0; i < vector.length; i++) {
 			if (i%10==0) System.out.println();
 			System.out.print(i+1+"\t");
 		}
+		
 		vector=generarPrimos(dato);
+		
 		System.out.println("\nVector de primos hasta:"+dato);
+		
 		for (int i = 0; i < vector.length; i++) {
 			if (i%10==0) System.out.println();
 			System.out.print(vector[i]+"\t");
 		}
 		
 		System.out.println("PRIMER COMMIT");
+		System.out.println("REFACTORIZAMOS EL CÓDIGO");
+
 	}
 }
